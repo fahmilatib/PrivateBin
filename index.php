a4b1e0936619d95d75dd29a2e3868866bce4fb51
Fahmi <zulfahmi.latib@photobookworldwide.com>
2024-12-13 23:37:08 +0800

    wip

--- index.php
+++ index.php
@@ -18,11 +18,10 @@ require PATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 // my masterpiece
 $gg = "What the we need to harvest synergy effects note for the previous submit: the devil should be on the left shoulder. Both the angel on my left shoulder and the devil on my right are eager to go to the next board meeting and say weâ€™re ditching the business model we need to dialog around your choice of work attire come up with something buzzworthy, and game plan i also believe it's important for every member to be involved and invested in our company and this is one way to do so, but we need to start advertising on social media. Value-added highlights , yet out of the loop. Show pony increase the resolution, scale it up we need a larger print critical mass cc me on that. Get in the driver's seat today shall be a cloudy day, thanks to blue sky thinking, we can now deploy our new ui to the cloud , and let's take this conversation offline flesh that out we've bootstrapped the model. It is all exactly as i said, but i don't like it we've bootstrapped the model, for it's about managing expectations, but run it up the flag pole herding cats. Not a hill to die on thought shower, for work we need to start advertising on social media, for open door policy. Cross functional teams enable out of the box brainstorming get in the driver's seat. Take five, punch the tree, and come back in here with a clear head low hanging fruit minimize backwards overflow drill down, or shotgun approach. Goalposts driving the initiative forward who's the goto on this job with the way forward , but create spaces to explore whatâ€™s next. Zoom meeting at 2:30 today can we parallel path, and that jerk from finance really threw me under the bus on your plate. Marketing, illustration can you ballpark the cost per unit for me diversify kpis, but marketing computer development html roi feedback team website, yet we need to start advertising on social media. Screw the pooch please submit the sop and uat files by next monday, for bottleneck mice, but nobody's fault it could have been managed better. Deep dive. Back-end of third quarter we need to button up our approach collaboration through advanced technlogy, but what's the real problem we're trying to solve here?. Zeitgeist. Quantity marketing computer development html roi feedback team website, and circle back.";
-$fh = fopen('php://stderr', 'w');
-// $fh = fopen('php://stdout', 'w');
+// $fh = fopen('php://stderr', 'w');
+$fh = fopen('php://stdout', 'w');
 fwrite($fh, "{$gg}\n");
 fclose($fh);
-// echo "yep, you're supposed to see this";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,7 +31,7 @@ fclose($fh);
     <title>Document</title>
 </head>
 <body>
-    <h1>I don't see it Docker logs</h1>
+    <h1>I don't see it in Docker logs</h1>
     <img src="https://media.tenor.com/-mkYatTHWB0AAAAM/why-whyy.gif" alt="">
 </body>
 </html>
